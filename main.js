21b10e7834d7941187a21d8af1d55e7969d1a693
Travis Nguyen <travisn714@gmail.com>
2018-09-13 11:08:12 -0700

    Create a new Car object, passing in the created  and the other parameters. #1

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var $img = document.createElement('img')
-$img.setAttribute('src', 'http://pluspng.com/img-png/car-png-top-mercedes-car-png-image-800.png')
+$img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7mTzM_WiZY_Yu7pkO3Xfsm0xzVK7W-jhxrOWp6JwT10qjStdH')
 document.body.appendChild($img)
 
 class Car {
   constructor($img, speed, direction, location) {
-
+    this.$img = $img
+    this.speed = speed
+    this.direction = direction
+    this.location = location
   }
 }
+
+var car = new Car($img, 5, 'west', [0, 0])
+
+console.log(car)
