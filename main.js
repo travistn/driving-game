415a42e58e1c66ac3104c40d549bccbf8870e945
Travis Nguyen <travisn714@gmail.com>
2018-09-13 10:59:43 -0700

    Create a variable that holds an img element and add it to the page. #1

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var $img = document.createElement('img')
+$img.setAttribute('src', 'http://pluspng.com/img-png/car-png-top-mercedes-car-png-image-800.png')
+document.body.appendChild($img)
+
 class Car {
-  constructor($img, speed, direction, location)
+  constructor($img, speed, direction, location) {
+
+  }
 }
